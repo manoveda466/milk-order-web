107eb4b8b59887512f0340025614544dd4951a4b
manoveda466 <30654681+manoveda466@users.noreply.github.com>
2025-11-07 05:42:18 +0000

    milk-order-web

---
+++ .angular/cache/19.2.19/milk-order-web/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,46 @@
+import {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-6WTUVQC3.js";
+import "./chunk-2O4WY5GE.js";
+import {
+  Dir
+} from "./chunk-SAT53URN.js";
+import "./chunk-6EAIWWHC.js";
+import "./chunk-XWUCXFVB.js";
+import "./chunk-S5HN7TTK.js";
+export {
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
---
+++ .angular/cache/19.2.19/milk-order-web/vite/deps/@angular_cdk_text-field.js
@@ -0,0 +1,16 @@
+import {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+} from "./chunk-5BMVQVZY.js";
+import "./chunk-6CWYHEIT.js";
+import "./chunk-6EAIWWHC.js";
+import "./chunk-XWUCXFVB.js";
+import "./chunk-S5HN7TTK.js";
+export {
+  AutofillMonitor,
+  CdkAutofill,
+  CdkTextareaAutosize,
+  TextFieldModule
+};
